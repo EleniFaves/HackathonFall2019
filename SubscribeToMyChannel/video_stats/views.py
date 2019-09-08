65b06870bf5b46407e2ced887606ba95b86eb218
mjenius <jamaledd@ualberta.ca>
2019-09-08 00:28:51 -0600

    Date is now Randomized

--- SubscribeToMyChannel/video_stats/views.py
+++ SubscribeToMyChannel/video_stats/views.py
@@ -14,9 +14,35 @@ class DayVideos(generic.ListView):
     model = Video
     template_name = "video_stats/index.html"
 
+    def getDate(self):
+
+        year = random.randint(2017,2018)
+
+        if year == 2017:
+            month = random.randint(11,12)
+            if month == 12:
+                day = random.randint(1,31)
+            else:
+                day = random.randint(14,30)
+        else:
+            month = random.randint(1,6)
+            if month == 4:
+                day = random.randint(1,30)
+            elif month == 6:
+                day = random.randint(1,14)
+            elif month == 2:
+                day = random.randint(1,28)
+            else:
+                day = random.randint(1,31)
+
+        return year, month, day
+
     def get(self, request, *args, **kwargs):
         print('PRINT', request.GET.get('year'))
-        context = {'videos' : Video.objects.filter(date=datetime.date(2018, 1, 1))}
+
+        year, month, day = self.getDate()
+
+        context = {'videos' : Video.objects.filter(date=datetime.date(year, month, day)), 'date' : datetime.date(year, month, day)}
         return render(request, self.template_name, context)
 
 class VideosByCategory(generic.DetailView):
